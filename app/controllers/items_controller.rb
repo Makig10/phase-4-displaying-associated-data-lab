4196f1d4a49b352323fca304e6f50421af47136e
Brian <mahuibrian14@gmail.com>
2023-06-30 23:16:41 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    # GET  /items
+    def index
+        items=Item.all 
+        render json:items, include: :users
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    # GET  /users/:id
+    def show
+        user=specific_user
+        render json:specific_user, include: :items
+    end
+    
+    private
+    def specific_user
+        User.find(id:params[:id])
+    end
+    
 end
